56a83482033fe772036c8d4cc0b3a00e1056c8b3
jibril <100243649+09jayy@users.noreply.github.com>
2024-06-14 18:03:55 +0100

    created user and transaction schema

---
+++ server/src/models/user.ts
@@ -0,0 +1,28 @@
+import mongoose, {Schema} from "mongoose"
+
+const TransactionSchema: Schema = new Schema({
+    date: {type: Date, required: true},
+    transType: String, 
+    des: String,
+    pay: { type: Number, required: true}
+})
+
+const userSchema: Schema = new Schema({
+    name: {
+        type: String, 
+        required: true
+    },
+    transations: {
+        type: [TransactionSchema]
+    }, 
+    dateCreated: {
+        type: Date, 
+        immutable: true,
+        default: () => Date.now()
+    }, 
+    balance: Number
+})
+
+const user = mongoose.model("User", userSchema)
+
+export default user
