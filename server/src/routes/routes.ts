ee5fdc1ea5f674713f8f0fc3926e71f41769991f
jibril <100243649+09jayy@users.noreply.github.com>
2024-06-14 18:13:49 +0100

    created routes.ts for routes using controller.ts

---
+++ server/src/routes/routes.ts
@@ -0,0 +1,8 @@
+import {Router} from "express"
+import {getUsers, addUser, findUser} from "../controllers/controller"
+
+const router: Router = Router()
+
+router.get("/users", getUsers)
+router.post("/users", addUser)
+router.post("users/login", findUser)
