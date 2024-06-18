4ffa4ffe2a20fc2045e269afd41873b011cb71a7
jibril <100243649+09jayy@users.noreply.github.com>
2024-06-18 23:18:32 +0100

    Created bank.ts for the bankSchema and model
    The purpose of this is to allow the user to split their finances virtually to seperate their money from one account rather than just simple 'balance: Number' field

---
+++ server/src/models/bank.ts
@@ -0,0 +1,23 @@
+import mongoose, {Schema, Model} from "mongoose"
+import { NextFunction } from "express"
+
+export interface IBank {
+    _id: String
+    name: String
+    balance: Number
+}
+
+export const bankSchema: Schema = new Schema<IBank>({
+    name: {
+        type: String,
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
+const Bank = mongoose.model("Bank", bankSchema)
+
+export default Bank
