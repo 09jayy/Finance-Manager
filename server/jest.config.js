2f494f98ea8c9b56e5e7bfe7f07b387512bf60a4
jibril <100243649+09jayy@users.noreply.github.com>
2024-06-21 18:48:07 +0100

    created first working unit test (get request all users)
    unit test successfully gets all users in correct format

---
+++ server/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/**/*.test.ts"],
+    verbose: true,
+    forceExit: true,
+    clearMocks: true,
+    resetMocks: true,
+    restoreMocks: true,
+    clearMocks:true,
+};
--- server/src/__tests__/user.test.ts
+++ server/src/__tests__/user.test.ts
@@ -1,11 +1,31 @@
-import {describe, expect, test} from '@jest/globals';
+import request from 'supertest';
+import app from '../server';
+import { describe, it, expect, beforeAll, afterAll } from '@jest/globals';
+import mongoose from 'mongoose';
+import config from '../config/config';
 
-function sum(a, b) {
-    return a + b;
-}
+let server;
 
-describe('sum module', () => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(3);
-    });
+beforeAll(async () => {
+    await mongoose.connect(config.mongoURI)
+
+    server = app.listen(config.port, () => {
+        console.log(`Test server running on port ${config.port}`)
+    })
+})
+
+afterAll(async () => {
+    // Close the server
+    await server.close()
+
+    // Disconnect from MongoDB
+    await mongoose.connection.close()
+})
+
+describe('GET /finance-manager/users', () => {
+    it('should return JSON and status 200', async () => {
+        const response = await request(app).get('/finance-manager/users')
+        expect(response.status).toBe(200)
+        expect(response.headers['content-type']).toMatch(/json/)
+    })
 });
