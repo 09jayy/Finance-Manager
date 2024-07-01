28d3a35a79c9d768e9b51fae8541aaba342179e1
jibril <100243649+09jayy@users.noreply.github.com>
2024-07-01 18:51:12 +0100

    added logout functionality in home page (auto includes redirects to login page)

--- app/pages/Home/HomePage.tsx
+++ app/pages/Home/HomePage.tsx
@@ -1,13 +1,24 @@
-import React from "react"
-import { View, Text } from "react-native"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import React, {Dispatch, SetStateAction, useContext} from "react"
+import { View, Text, Pressable } from "react-native"
+import {loginContext} from "../../appContext"
+
+const logout = async (setLoggedIn: Dispatch<SetStateAction<boolean>>) => {
+    await AsyncStorage.removeItem("token")
+    setLoggedIn(false)
+}
 
 export const Home = () => {
-    console.log("render pls")
+    const {setLoggedIn} = useContext(loginContext)
+
     return (
         <View>
             <Text>
                 Hello there
             </Text>
+            <Pressable onPress={() => logout(setLoggedIn as Dispatch<SetStateAction<boolean>>)}>
+                <Text>Logout</Text>
+            </Pressable>
         </View>
     )
 }
