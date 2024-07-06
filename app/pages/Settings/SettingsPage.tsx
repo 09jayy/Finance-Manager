c2dfda558a54816d64723bd30902fe9e670af62e
jibril <100243649+09jayy@users.noreply.github.com>
2024-07-06 02:48:27 +0100

    added loading animation activity

--- app/pages/Settings/SettingsPage.tsx
+++ app/pages/Settings/SettingsPage.tsx
@@ -1,23 +1,33 @@
 import {useState, useEffect, SetStateAction, Dispatch} from "react"
-import {View, Text, SafeAreaView} from "react-native"
+import {View, Text, SafeAreaView, ActivityIndicator} from "react-native"
 import {getUserData, UserData} from "./functions"
 
 export const SettingsPage = () => {
     const [userData, setUserData] : [UserData, Dispatch<SetStateAction<UserData>>] = useState({name: "", email: ""})
+    const [loading, setLoading]= useState(true)
 
     useEffect( () => {
         getUserData().then(data => {
             setUserData(data)
+            setLoading(false)
         }).catch( (error: Error) => {
-            console.log(error )
+            console.log(error)
         })
     }, [])
 
     return (
         <SafeAreaView>
             <Text>Account</Text>
-            <Text>{userData.name}</Text>
-            <Text>{userData.email}</Text>
+            {
+                loading ? (
+                    <ActivityIndicator/> 
+                ) : (
+                    <View>
+                        <Text>{userData.name}</Text>
+                        <Text>{userData.email}</Text>
+                    </View>
+                )
+            }
         </SafeAreaView>
     )
 }
