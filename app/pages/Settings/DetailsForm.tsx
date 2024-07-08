d00a6c54628246b211a2b09b59c2e8faae42a24f
jibril <100243649+09jayy@users.noreply.github.com>
2024-07-08 02:01:33 +0100

    added styling to form, added error message

--- app/pages/Settings/DetailsForm.tsx
+++ app/pages/Settings/DetailsForm.tsx
@@ -1,5 +1,5 @@
 import {useContext, useState} from "react"
-import { Text, TextInput, SafeAreaView, TouchableOpacity, StyleSheet, Dimensions } from "react-native"
+import { Text, TextInput, SafeAreaView, TouchableOpacity, StyleSheet, Dimensions, View } from "react-native"
 import {settingsContext} from "./SettingsContext"
 import {submitDetails} from "./functions/detailsFormFunctions"
 
@@ -13,6 +13,7 @@ export interface DetailsType {
 
 export const DetailsForm = () => {
     const {userData, setUserData} = useContext(settingsContext)
+    const [submitError, setSubmitError] = useState("")
 
     const [details, setDetails] = useState<DetailsType>({
         name: "",
@@ -27,35 +28,65 @@ export const DetailsForm = () => {
     }
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Name: </Text>
             <TextInput placeholder={userData!.name} value={details.name} onChangeText={val => updateDetail("name", val)} style={styles.input}/>
 
             <Text style={styles.title}>Email: </Text>
             <TextInput placeholder={userData!.email} value={details.email} onChangeText={val => updateDetail("email", val)} style={styles.input}/>
 
-            <Text style={styles.title}>Password</Text>
+            <Text style={styles.title}>Password: </Text>
             <TextInput placeholder="Current Password..." value={details.currentPassword} onChangeText={val => updateDetail("currentPassword",val)} style={styles.input}/>
             <TextInput placeholder="New Password..." value={details.newPassword} onChangeText={val => updateDetail("newPassword", val)} style={styles.input}/>
             <TextInput placeholder="Confirm Password..." value={details.confirmPassword} onChangeText={val => updateDetail("confirmPassword", val)} style={styles.input}/>
 
-            <TouchableOpacity onPress={() => submitDetails(details)}>
-                <Text>Submit</Text>
-            </TouchableOpacity>
+            <Text style={styles.submitError}>{submitError}</Text>
+
+            <View style={styles.submitContainer}>
+                <TouchableOpacity onPress={() => submitDetails(details, setSubmitError)} style={styles.submit}>
+                    <Text style={styles.submitText}>SUBMIT</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     input: {
         width: Dimensions.get("window").width, 
         height: 30,
         backgroundColor: "white", 
         fontSize: 16, 
-        paddingLeft: 10
+        paddingLeft: 30,
+        marginBottom: 10,
     },
     title: {
         fontSize: 18, 
-        margin: 5
+        margin: 5, 
+        marginLeft: 15
+    },
+    submitContainer: {
+        flex: 1,
+        alignItems: "center",  
+    },
+    submit: {
+        backgroundColor: "#1776e3",
+        borderRadius: 5,  
+        width: 230,
+        height: 30, 
+        justifyContent: "center", 
+        alignItems: "center", 
+    },
+    submitText: {
+        color: "white",
+        fontSize: 16,
+    },
+    submitError: {
+        color: "#d12304",
+        textAlign: "center",
+        padding: 10
     }
 })
--- app/pages/Settings/functions/detailsFormFunctions.ts
+++ app/pages/Settings/functions/detailsFormFunctions.ts
@@ -1,5 +1,7 @@
 import {DetailsType} from "../DetailsForm"
+import {isEmailValid, isPasswordValid, PasswordError} from "../../../functions/validation"
+import { Dispatch, SetStateAction } from "react"
 
-export const submitDetails = (details: DetailsType) => {
-    console.log(details)
+
+export const submitDetails = (details: DetailsType, setSubmitError: Dispatch<SetStateAction<string>>) => {
 }
