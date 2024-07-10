1dbf5168180393c8cd773a97956119137aa8bc33
jibril <100243649+09jayy@users.noreply.github.com>
2024-07-10 18:34:27 +0100

    created effect and function to get bank data upon mount of bankspage

--- app/pages/Bank/BanksPage.tsx
+++ app/pages/Bank/BanksPage.tsx
@@ -1,9 +1,21 @@
 import { View, Text } from "react-native"
+import {useState, useEffect} from "react"
+import {getBankData} from "./functions/banksPageFunction"
 
 export const BanksPage = () => {
+    const [banks, setBanks] = useState([])
+
+    useEffect(()=> {
+        getBankData()
+        .then(data => {
+            console.log(data)
+        })
+    },[])
+
     return (
         <View>
             <Text>Banks Page</Text>
+            <Text>{banks}</Text>
         </View>
     )
 }
---
+++ app/pages/Bank/functions/banksPageFunction.ts
@@ -0,0 +1,36 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import {API_URL} from "react-native-dotenv"
+
+type Bank = {
+    _id: string
+    name: string
+    balance: number
+}
+
+type Banks = {
+    banks: [Bank]
+}
+
+export const getBankData = async () => {
+    const token = await AsyncStorage.getItem("token")
+
+    const request = {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    }
+
+    return fetch(`http://${API_URL}/finance-manager/banks/get`, request)
+        .then(response => {
+            if (!response.ok){
+                throw new Error(`${response.text()}`)
+            }
+
+            return response.json()
+        }).then( (data: Banks) => {
+            return data.banks
+        }).catch((error: Error) => {
+            console.log(error)
+        })
+}
