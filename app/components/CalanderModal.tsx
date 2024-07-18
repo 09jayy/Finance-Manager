b3fcd57e3f405168174b0c0c710ca2e12e2784b7
jibril <100243649+09jayy@users.noreply.github.com>
2024-07-19 00:41:21 +0100

    calandar modal styling close button to right corner for consistency - added initial date to be reverted to upon cancellation

--- app/components/CalanderModal.tsx
+++ app/components/CalanderModal.tsx
@@ -1,8 +1,9 @@
 import {Modal, View, StyleSheet, Pressable, Text, TouchableOpacity} from "react-native"
 import DateTimePicker, { DateType } from "react-native-ui-datepicker"
 import dayjs from "dayjs"
-import { Dispatch, SetStateAction, useEffect, useState } from "react"
+import { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react"
 import { AntDesign } from "@expo/vector-icons"
+import { useFocusEffect } from "@react-navigation/native"
 
 type CalandarModalProps = {
     modalVisible: boolean
@@ -12,6 +13,12 @@ type CalandarModalProps = {
 }
 
 export const CalandarModal = ({modalVisible, setModalVisible, date, setDate}: CalandarModalProps) => {
+    // initial date before modification is recorded so that if the user cancels - the date is set back to original date
+    const [initialDate, setInitialDate] = useState(dayjs())
+    useFocusEffect(useCallback(() => {
+        setInitialDate(date)
+    }, [modalVisible]))
+
     return (
         <View style={styles.parent}>
             <Modal
@@ -19,19 +26,20 @@ export const CalandarModal = ({modalVisible, setModalVisible, date, setDate}: Ca
                 visible={modalVisible}
                 transparent={true}
                 onRequestClose={() => {
+                    setDate(initialDate)
                     setModalVisible(false)
                 }}
             >
                 <View style={styles.modalOverlay}>
                     <View style={styles.modalContent}>
-                            <Pressable onPress={() => setModalVisible(false)}>
+                            <Pressable style={{marginLeft: "auto", margin: 5}} onPress={() => {setDate(initialDate); setModalVisible(false)}}>
                                 <AntDesign name="close" size={24} color="black"/>
                             </Pressable>
 
                         <DateTimePicker mode="single" date={date.toDate()} onChange={(params) => {setDate(dayjs(params.date).add(1, "hour"))}}/>
 
                         <TouchableOpacity onPress={()=>{setModalVisible(false)}} style={styles.confirmBtn}>
-                            <Text style={styles.confirmText}>CONFIRM</Text>
+                                <Text style={styles.confirmText}>CONFIRM</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
